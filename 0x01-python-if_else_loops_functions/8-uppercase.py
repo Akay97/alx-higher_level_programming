f2114e88965d711b2b538fd7ede09f2095a66fbb
Akala Moses <akoredeolamilekan97@gmail.com>
2024-01-02 15:17:30 -0800

    print uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        uppercase_char = chr(ord(c) - 32) if 'a' <= c <= 'z' else c
+        print("{}".format(uppercase_char), end="")
+    print()
