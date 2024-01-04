f18abd44f49d4f283d851912f99a2831f9fb668b
Akala Moses <akoredeolamilekan97@gmail.com>
2024-01-04 09:29:33 -0800

    python import module

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation import add, sub
+
+    if a < b:
+        d = add(a, b)
+        for i in range(4, 6):
+            d = add(d, i)
+            return (c)
+    else:
+        return (sub(a, b))
