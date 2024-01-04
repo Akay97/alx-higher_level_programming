d0be6f21caee2b72f814c2c669fb6f5bacca49a2
Akala Moses <akoredeolamilekan97@gmail.com>
2024-01-04 08:57:24 -0800

    my import module project

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == '__main__':
+    import hidden_4
 
-string = dir(hidden_4)
-for name in string:
-    if name[:2] != "__":
-        print(name)
+    string = dir(hidden_4)
+    for name in string:
+        if name[:2] != "__":
+            print(name)
