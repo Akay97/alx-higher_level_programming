efa3bca16482bd6112de790a2cade41f0735cf6e
Akala Moses <akoredeolamilekan97@gmail.com>
2024-01-04 08:54:41 -0800

    my import module project

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == '__main__':
+    import sys
 
-arg = len(sys.argv) - 1
-if arg == 0:
-    print("0 arguments.")
-elif arg == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(arg))
-for num in range(arg):
-    print("{}: {}".format(num + 1, sys.argv[num + 1]))
+    arg = len(sys.argv) - 1
+    if arg == 0:
+        print("0 arguments.")
+    elif arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arg))
+    for num in range(arg):
+        print("{}: {}".format(num + 1, sys.argv[num + 1]))
