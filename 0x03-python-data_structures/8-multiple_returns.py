f26907cccab711d794ae565592e1f9a93e485c74
Akala Moses <akoredeolamilekan97@gmail.com>
2024-01-07 05:09:28 -0800

    python Data structure

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if not sentence:
-        sentence[0] = None
-    else:
-        read = len(sentence)
-        first = sentence[0]
-        return read, first
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
