2f38b22a89fef641996bb2f1fecece39000b73f8
Akala Moses <akoredeolamilekan97@gmail.com>
2024-01-07 04:57:01 -0800

    python Data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-    new_string =""
-    for lit in my_string:
-        if lit != "c" and lit != "C":
-            new_string += lit
-    return new_string
+    result = ""
+    for char in my_string:
+        if char.lower() != 'c':
+            result += char
+    return result
