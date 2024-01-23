17accf65ec30b74079211e8d74fe573c48151b87
Akala Moses <akoredeolamilekan97@gmail.com>
2024-01-23 12:51:48 -0800

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,8 +5,7 @@ class Square:
     """Define a square."""
 
     def __init__(self, size):
-        """
-        Constructor.
+        """Constructor.
 
         Args:
             size: The size of the square.
